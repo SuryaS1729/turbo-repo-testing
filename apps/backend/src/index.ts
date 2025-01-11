11527dfb780ec1e9e0508d8922e8456ca1772cfd
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-01-11 23:25:07 +0530

    completed the turborepo, had to use ESBUILD

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,3 @@
-import express from "express"
+import {BACKEND_URL} from "@repo/common/config"
 
-const app = express()
-
-app.get("/",(req,res)=>{
-    res.json({
-        message: "hello world"
-    })
-})
-
-app.listen(3000);
+console.log(BACKEND_URL)
--- packages/commons/src/index.ts
+++
@@ -1,5 +0,0 @@
-
-
-export const VALUE = " SAHITHI";
-
-export const BACKEND_URL = "https:/api.google.com"
